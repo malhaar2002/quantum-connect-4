f27b9702242a9c2d79edaf91721e305ed78dac8d
malhaar2002 <aroramalhaar@gmail.com>
2024-05-24 12:13:29 +0530

    modified constants

--- constants.py
+++ constants.py
@@ -4,7 +4,7 @@ RED = (255,0,0)
 YELLOW = (255,255,0)
 SQUARESIZE = 150
 RADIUS = int(SQUARESIZE/2 - 15)
-SERVER_IP = "192.168.102.33"
+SERVER_IP = "192.168.1.10"
 SERVER_PORT = 5555
 
 # do not change unless you modified game.py accordingly
